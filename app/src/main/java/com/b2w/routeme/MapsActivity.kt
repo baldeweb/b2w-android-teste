1c9a173f53e7db7325d1a191588ec1c2d2ff01f3
Wallace <wallacecorin@gmail.com>
2020-04-13 23:25:59 -0300

    autocomplete com a pesquisa do maps, falta ajustar a validação de permissões e fazer perfumarias no layout

--- app/src/main/java/com/b2w/routeme/MapsActivity.kt
+++ app/src/main/java/com/b2w/routeme/MapsActivity.kt
@@ -1,13 +1,17 @@
 package com.b2w.routeme
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -17,23 +21,49 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.google.maps.DirectionsApi
 import com.google.maps.DirectionsApiRequest
 import com.google.maps.GeoApiContext
 import com.google.maps.model.*
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val REQUEST_PERMISSION_LOCATION = 101
+    private val AUTOCOMPLETE_REQUEST_CODE = 1
     private var mLocation: Location? = null
+    private var latitudeFromAutocomplete : Double = 0.0
+    private var longitudeFromAutocomplete : Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        initialSetupMap()
+    }
+
+    private fun initialSetupMap() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        if (!Places.isInitialized())
+            Places.initialize(applicationContext, getString(R.string.google_maps_key), Locale.getDefault());
+    }
+
+    private fun setupAutoCompletePlaces() {
+        val fields= listOf(Place.Field.ID,Place.Field.NAME,Place.Field.LAT_LNG)
+        val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields).build(this)
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
@@ -46,7 +76,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val currentLocation = LatLng(latLng.latitude, latLng.longitude)
         mMap.addMarker(MarkerOptions().position(currentLocation))
 
-        val targetLocation = LatLng(-23.5415905, -46.8000877)
+        val targetLocation = LatLng(latitudeFromAutocomplete, longitudeFromAutocomplete)
         mMap.addMarker(MarkerOptions().position(targetLocation))
 
         val path: MutableList<LatLng> = ArrayList()
@@ -59,7 +89,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .getDirections(
                 context,
                 "${latLng.latitude},${latLng.longitude}",
-                "-23.5415905,-46.8000877"
+                "$latitudeFromAutocomplete,$longitudeFromAutocomplete"
             )
 
         try {
@@ -143,6 +173,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                val place = data?.let { Autocomplete.getPlaceFromIntent(it) };
+
+                latitudeFromAutocomplete = place?.latLng?.latitude ?: 0.0
+                longitudeFromAutocomplete = place?.latLng?.longitude ?: 0.0
+
+                mMap.clear()
+                getCurrentPosition()
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                val status = data?.let { Autocomplete.getStatusFromIntent(it) }
+                Log.d("LOG", status?.statusMessage ?: "");
+            }
+        }
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -158,6 +206,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
         }
+    }
+
+    fun onSearchLocation(v: View) {
+        setupAutoCompletePlaces()
     }
 
 }
